d759201b07defc47187daa806937374b47e749fd
leandromenoci <menoci.leandro@gmail.com>
2022-05-20 18:00:58 -0300

    CSS-in-JS

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,39 @@
 import Link from '../src/components/Link'
+import React from 'react'
+
+function GlobalStyle() {
+  return (
+    <style global jsx>{`
+      body {
+        font-family: sans-serif;
+        background: #CCC;
+      }
+    `}</style>
+  )
+}
+
+function Title({ children, as }) {
+  const Tag = as
+  return (
+    <React.Fragment>
+      <Tag>
+        {children}
+      </Tag>
+      <style jsx>{`
+        ${Tag} {
+          color: red;
+          
+        }
+      `}</style>
+    </React.Fragment>
+  )
+}
 
 export default function HomePage() {
   return (
     <div>
-      <h1>Alura Cases - Home</h1>
+      <GlobalStyle />
+      <Title as='h1'>Alura Cases - Home</Title>
       <Link href='/faq'>
         Ir para a página do FAQ
       </Link>
