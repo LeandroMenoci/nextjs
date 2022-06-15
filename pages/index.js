151cbd59f559884398a174a17a25fe1598d24ab9
leandromenoci <menoci.leandro@gmail.com>
2022-06-14 22:38:55 -0300

    Estilização no Next.js / Arquitetura de aplicação / Lidar com arquivos estáticos

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,38 @@
-import Link from '../src/components/Link'
-import React from 'react'
+import HomeScreen from "../src/screens/HomeScreen";
 
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      body {
-        font-family: sans-serif;
-        background: #CCC;
-      }
-    `}</style>
-  )
-}
+export default HomeScreen
 
-function Title({ children, as }) {
-  const Tag = as
-  return (
-    <React.Fragment>
-      <Tag>
-        {children}
-      </Tag>
-      <style jsx>{`
-        ${Tag} {
-          color: red;
+// import Link from '../src/components/Link'
+// import React from 'react'
+// import Footer from '../src/components/patterns/Footer'
+
+
+
+// function Title({ children, as }) {
+//   const Tag = as
+//   return (
+//     <React.Fragment>
+//       <Tag>
+//         {children}
+//       </Tag>
+//       <style jsx>{`
+//         ${Tag} {
+//           color: red;
           
-        }
-      `}</style>
-    </React.Fragment>
-  )
-}
+//         }
+//       `}</style>
+//     </React.Fragment>
+//   )
+// }
 
-export default function HomePage() {
-  return (
-    <div>
-      <GlobalStyle />
-      <Title as='h1'>Alura Cases - Home</Title>
-      <Link href='/faq'>
-        Ir para a página do FAQ
-      </Link>
-    </div>
-  )
-}
+// export default function HomePage() {
+//   return (
+//     <div>
+//       <Title as='h1'>Alura Cases - Home</Title>
+//       <Link href='/faq'>
+//         Ir para a página do FAQ
+//       </Link>
+//       <Footer />
+//     </div>
+//   )
+// }
